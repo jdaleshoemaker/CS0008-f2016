9993edc53380e898e0ef2fa960f8ac870ba76340
jdaleshoemaker <jdaleshoemaker@gmail.com>
2016-09-13 15:42:15 -0400

    I typed something

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,9 @@
+name: John Dale Shoemaker
+email: jdaleshoemaker@gmail.com
+date: 9/13/16
+class: CS0008 F2016
+instructor: max novelli (man8@pitt.edu)
+description: Starting with Python, Chapter 1, exercise 3
+notes: I am very unsure if I am doing this correctly. It's my first time using python' \
+
+
